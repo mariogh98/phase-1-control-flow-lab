2e842fc2e14bbaf32e19488206eb08d8ffd61879
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-05-17 18:00:31 -0500

    commit tree fa58180544852f342d827682923556d591a0b894

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberOfFeet){
+  if (numberOfFeet <= 400) {
+    return "This one is on me!"
+  } else if (numberOfFeet > 400 && numberOfFeet <= 2000) {
+    return "That will be twenty bucks."
+  } else if (numberOfFeet > 2000 && numberOfFeet<=2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (numberOfFeet > 2500) {
+    return "No can do."
+  }
+}  
+
+function ternaryCheckCity(city){
+ const cityResponse = (city === "NYC") ? ("Ok, sounds good.") : ("No go.")
+ return cityResponse
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous" :
+      return "Thank you so much."
+    case "not as generous" :
+      return "Thank you." 
+    default:
+      return "Bye."   
+  } 
 }
